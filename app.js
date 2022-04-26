fe515d8fac98a71f2b47d12f25823147346021ef
andriusmw <andriusmw@yahoo.es>
2022-04-26 12:20:55 +0200

    muestra temperatura en el DOM

--- app.js
+++ app.js
@@ -19,13 +19,35 @@ window.addEventListener(`load`, () => {
       //para después introducirlas en la llamada a la api.
 
       //ubicación actual
-      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c44fb8c6c479a763a22b7b7d70c5f610`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c44fb8c6c479a763a22b7b7d70c5f610&units=metric`;
       //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
       console.log(url);
 
       //ubicación por nombre ciudad
       // const url = https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+      //*******************HACEMOS FETCH PARA VER LA RESPUESTA DE LA API**********************
+      /*
+      /************************************************************************************** */
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        //Es una promesa que nos devuelve el json de la respuesta
+        .then((data) => {
+          console.log(data);
+          //Muestra por consola la respuesta, está con la promesa anterior
+          let temp = data.main.temp;
+          temperaturaValor.textContent = temp;
+          //pasamos a la variable temperatura valor, que es el elemento que tenga
+          //ese id, el valor de data.main.temp en el json, que es la temperatura
+          //que hace en esa respuesta de la api.
+        })
+        .catch((error) => {
+          console.log(error);
+          //Prometemos capturar el error si nos da y lo mostramos por consola
+        });
     });
   }
 });
