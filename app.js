8aada78b3c9e999f3602642f088c5d627b3692c7
andriusmw <andriusmw@yahoo.es>
2022-04-26 12:08:23 +0200

    capturamos elementos del DOM

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 window.addEventListener(`load`, () => {
   let lon;
   let lat;
+
+  //capturamos elementos del DOM
+  let temperaturaValor = document.getElementById("temperaturaValor");
+  let temperaturaDescrip = document.getElementById("temperaturaDescrip");
+  let ubicacion = document.getElementById("ubicacion");
+  let iconoAnimado = document.getElementById("iconoAnimado");
+  let velocidadViento = document.getElementById("velocidadViento");
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((posicion) => {
       //console.log(posicion);
@@ -10,12 +18,14 @@ window.addEventListener(`load`, () => {
       //asignamos nuestra longitud y latitud a las variables lat y lon
       //para después introducirlas en la llamada a la api.
 
+      //ubicación actual
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c44fb8c6c479a763a22b7b7d70c5f610`;
       //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
       console.log(url);
-      //pasamos la longitud y latitud de nuestra ubicacion a la url de la api, con la apikey
-      //y la mostramos por consola.
+
+      //ubicación por nombre ciudad
+      // const url = https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     });
   }
 });
